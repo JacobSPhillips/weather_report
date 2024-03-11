284d2504301866125f968b9a2925683e28b1b9c0
Jacob Phillips <jsp452@msstate.edu>
2024-03-11 13:18:24 -0500

    Update weather_report.py
    added an if else statement that checks if location is valid

--- weather_report.py
+++ weather_report.py
@@ -1,4 +1,5 @@
 from requests import get
+from requests import status_codes
 from pyfiglet import figlet_format
 
 print(figlet_format("WEATHER REPORT"))  # prints title
@@ -8,20 +9,24 @@ city = input("What city do you live in: ")  # user inputs a city for the website
 url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 
 res = get(url)  # requests the information of the URL
-data = res.json()  # changes the information to accessible python
 
-weather = data["weather"][0]["description"]  # creates a variable for the description of the weather
-# temp is a dict of all the relevant data converted to Fahrenheit
-temp = {"Temperature": round((data["main"]["temp"] - 273.15) * 9 / 5 + 32, 2),
-        "Feels like": round((data["main"]["feels_like"] - 273.15) * 9 / 5 + 32, 2),
-        "High": round((data["main"]["temp_max"] - 273.15) * 9 / 5 + 32, 2),
-        "Low": round((data["main"]["temp_min"] - 273.15) * 9 / 5 + 32, 2),
-        "Humidity": round((data["main"]["humidity"]))
-        }
+if status_codes == 200:
+    data = res.json()  # changes the information to accessible python
 
-print(f'Temperature(Fahrenheit): {temp["Temperature"]}')
-print(f'Feels like: {temp["Feels like"]}')
-print(f'High: {temp["High"]}')
-print(f'Low: {temp["Low"]}')
-print(f'Humidity: {temp["Humidity"]}%\n')
-print(f'description: {weather}')
+    weather = data["weather"][0]["description"]  # creates a variable for the description of the weather
+    # temp is a dict of all the relevant data converted to Fahrenheit
+    temp = {"Temperature": round((data["main"]["temp"] - 273.15) * 9 / 5 + 32, 2),
+            "Feels like": round((data["main"]["feels_like"] - 273.15) * 9 / 5 + 32, 2),
+            "High": round((data["main"]["temp_max"] - 273.15) * 9 / 5 + 32, 2),
+            "Low": round((data["main"]["temp_min"] - 273.15) * 9 / 5 + 32, 2),
+            "Humidity": round((data["main"]["humidity"]))
+            }
+
+    print(f'Temperature(Fahrenheit): {temp["Temperature"]}')
+    print(f'Feels like: {temp["Feels like"]}')
+    print(f'High: {temp["High"]}')
+    print(f'Low: {temp["Low"]}')
+    print(f'Humidity: {temp["Humidity"]}%\n')
+    print(f'description: {weather}')
+else:
+    print("error: could not load location.")
