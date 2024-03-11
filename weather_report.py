da8df50a7fa1e09efff19b94a435e6dd99fd59db
Jacob Phillips <jsp452@msstate.edu>
2024-03-11 09:26:13 -0500

    Add files via upload

---
+++ weather_report.py
@@ -0,0 +1,27 @@
+import requests
+import pyfiglet
+
+print(pyfiglet.figlet_format("WEATHER REPORT"))  # prints title
+api_key = "348f97246e54ba434c5e50142310d2a2"  # key used to access websites API
+city = input("What city do you live in: ")  # user inputs a city for the website to use
+# f string as the URL that changes based on the city
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+res = requests.get(url)  # requests the information of the URL
+data = res.json()  # changes the information to accessible python
+
+weather = data["weather"][0]["description"]  # creates a variable for the description of the weather
+# temp is a dict of all the relevant data converted to Fahrenheit
+temp = {"Temperature": round((data["main"]["temp"] - 273.15) * 9 / 5 + 32, 2),
+        "Feels like": round((data["main"]["feels_like"] - 273.15) * 9 / 5 + 32, 2),
+        "High": round((data["main"]["temp_max"] - 273.15) * 9 / 5 + 32, 2),
+        "Low": round((data["main"]["temp_min"] - 273.15) * 9 / 5 + 32, 2),
+        "Humidity": round((data["main"]["humidity"]))
+        }
+
+print(f'Temperature(Fahrenheit): {temp["Temperature"]}')
+print(f'Feels like: {temp["Feels like"]}')
+print(f'High: {temp["High"]}')
+print(f'Low: {temp["Low"]}')
+print(f'Humidity: {temp["Humidity"]}%\n')
+print(f'description: {weather}')
