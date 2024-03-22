4437a2892b4376f13f03c316444d902aa7558a01
Jacob Phillips <jsp452@msstate.edu>
2024-03-22 08:55:09 -0500

    Update weather_report.py
    Fixing bug and improving usability

--- weather_report.py
+++ weather_report.py
@@ -1,32 +1,36 @@
 from requests import get
-from requests import status_codes
 from pyfiglet import figlet_format
 
 print(figlet_format("WEATHER REPORT"))  # prints title
-api_key = "348f97246e54ba434c5e50142310d2a2"  # key used to access websites API
-city = input("What city do you live in: ")  # user inputs a city for the website to use
-# f string as the URL that changes based on the city
-url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 
-res = get(url)  # requests the information of the URL
+loop = 'yes'
+while loop == "y" or loop == "yes":
+    api_key = "ee0e75237c8ad666eeb3275fd07dc49a"  # key used to access websites API
+    city = input("What city do you live in: ")  # user inputs a city for the website to use
+    # f string as the URL that changes based on the city
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
 
-if status_codes == 200:
-    data = res.json()  # changes the information to accessible python
+    res = get(url)  # requests the information of the URL
 
-    weather = data["weather"][0]["description"]  # creates a variable for the description of the weather
-    # temp is a dict of all the relevant data converted to Fahrenheit
-    temp = {"Temperature": round((data["main"]["temp"] - 273.15) * 9 / 5 + 32, 2),
-            "Feels like": round((data["main"]["feels_like"] - 273.15) * 9 / 5 + 32, 2),
-            "High": round((data["main"]["temp_max"] - 273.15) * 9 / 5 + 32, 2),
-            "Low": round((data["main"]["temp_min"] - 273.15) * 9 / 5 + 32, 2),
-            "Humidity": round((data["main"]["humidity"]))
-            }
+    if res.status_code == 200:
+        data = res.json()  # changes the information to accessible python
 
-    print(f'Temperature(Fahrenheit): {temp["Temperature"]}')
-    print(f'Feels like: {temp["Feels like"]}')
-    print(f'High: {temp["High"]}')
-    print(f'Low: {temp["Low"]}')
-    print(f'Humidity: {temp["Humidity"]}%\n')
-    print(f'description: {weather}')
-else:
-    print("error: could not load location.")
+        weather = data["weather"][0]["description"]  # creates a variable for the description of the weather
+        # temp is a dict of all the relevant data converted to Fahrenheit
+        temp = {"Temperature": round((data["main"]["temp"] - 273.15) * 9 / 5 + 32, 2),
+                "Feels like": round((data["main"]["feels_like"] - 273.15) * 9 / 5 + 32, 2),
+                "High": round((data["main"]["temp_max"] - 273.15) * 9 / 5 + 32, 2),
+                "Low": round((data["main"]["temp_min"] - 273.15) * 9 / 5 + 32, 2),
+                "Humidity": round((data["main"]["humidity"]))
+                }
+
+        print(f'Temperature(Fahrenheit): {temp["Temperature"]}')
+        print(f'Feels like: {temp["Feels like"]}')
+        print(f'High: {temp["High"]}')
+        print(f'Low: {temp["Low"]}')
+        print(f'Humidity: {temp["Humidity"]}%\n')
+        print(f'description: {weather}\n')
+    else:
+        print("error: could not load location.")
+
+    loop = input("Would you like to go again ('yes' or 'no'):")
